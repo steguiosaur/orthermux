61ab9bc3e7fc3b2af3e60adcbe7bd2177a5f5ac5
steguiosaur <91378710+steguiosaur@users.noreply.github.com>
2022-10-30 14:29:37 +0800

    fix config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 
 ########### ALIASES
 source ~/.config/sources/aliases
-alias arch="proot-distro login --isolated --user steve archlinux"
+#alias arch="proot-distro login --isolated --user steve archlinux"
 
 ########### EXPORTS
 # TERMINAL DEFAULTS
@@ -16,10 +16,10 @@ export LINK="https://github.com/steguiosaur"
 export LINK_SSH="git@github.com:steguiosaur"
 
 # JDK JAVA
-export ANDROID_SDK_ROOT="~/Android/sdk"
-export PATH="$PATH:$ANDROID_SDK_ROOT/cmdline-tools/latest/bin"
-export JAVA_HOME="$PREFIX/opt/openjdk"
-export PATH="$PATH:$ANDROID_SDK_ROOT/build-tools/30.0.3"
+#export ANDROID_SDK_ROOT="~/Android/sdk"
+#export PATH="$PATH:$ANDROID_SDK_ROOT/cmdline-tools/latest/bin"
+#export JAVA_HOME="$PREFIX/opt/openjdk"
+#export PATH="$PATH:$ANDROID_SDK_ROOT/build-tools/30.0.3"
 
 ########### AUTOSTART
 source ~/.config/sources/autostart
